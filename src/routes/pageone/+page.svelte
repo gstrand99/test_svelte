c5812cebeaa39e0a8ce8c6fb8ffa9852c9c4234f
gstrand99 <gstrand99@gmail.com>
2023-09-22 17:22:47 -0500

    test

---
+++ src/routes/pageone/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import wallpaper_1 from "$lib/assets/wallpaper_1.webp";
+</script>
+
+<div
+  class="h-screen bg-center bg-no-repeat bg-cover"
+  style="background-image: url({wallpaper_1});"
+>
+  <a href="/">HOME</a>
+  <a href="/pagetwo">Page One</a>
+</div>
---
+++ src/routes/pagetwo/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import wallpaper_2 from "$lib/assets/wallpaper_2.webp";
+</script>
+
+<div
+  class="h-screen bg-center bg-no-repeat bg-cover"
+  style="background-image: url({wallpaper_2});"
+>
+  <a href="/">HOME</a>
+  <a href="/pageone">Page One</a>
+</div>
